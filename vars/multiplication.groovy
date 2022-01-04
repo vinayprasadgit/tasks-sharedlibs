0c1936929f974a97e7811044f116846fc2d649ef
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-04 18:46:57 +0530

    Update multiplication.groovy

--- vars/multiplication.groovy
+++ vars/multiplication.groovy
@@ -2,5 +2,6 @@ def call(x,y)
 {
   def content = libraryResource "com/math/mul/resMul.sh"
   writeFile file: "resMul.sh", text: content
+  sh "chmod a+x ./resMul.sh"
   sh "./resMul.sh ${x} ${y}"
 }
