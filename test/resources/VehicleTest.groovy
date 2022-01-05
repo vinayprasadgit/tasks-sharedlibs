2bb51aa28e98d8dc4ab453aca2f874bbba70a757
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-05 18:07:38 +0530

    Create VehicleTest.groovy

---
+++ test/resources/VehicleTest.groovy
@@ -0,0 +1,12 @@
+package test.resources
+import groovy.test.GroovyTestCase
+import com.demo.Vehicle
+
+class VehicleTest extends GroovyTestCase {
+    void testDisplay() {
+        def v1 = new Vehicle("Benz","Black")
+        def expected = "Benz","Black"
+        assertToString(stud.Display(), expected)
+        println("All Tests Are Passed")
+    }
+}
