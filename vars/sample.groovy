f2b3139004568c1b709b198ccd71d752bc2d36e2
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-05 13:04:12 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,7 +1,10 @@
 def call()
 {
-  def v1 = new Vehicle("Benz","Black")
-  printf v1.getName()
-  printf v1.getColor()
+  void run(){
+    def v1 = new Vehicle("Benz","Black")
+    printf v1.getName()
+    printf v1.getColor()
+  }
+  run()
 }
   
