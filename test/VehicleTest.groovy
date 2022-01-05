4c4c88577e437d52238045c2e0712807e18682ca
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-05 17:45:15 +0530

    Update VehicleTest.groovy

--- test/VehicleTest.groovy
+++ test/VehicleTest.groovy
@@ -5,7 +5,7 @@ import com.demo.Vehicle
 class VehicleTest extends GroovyTestCase {
     void testDisplay() {
         def v1 = new Vehicle("Benz","Black")
-        def expected = "Benz","Green"
+        def expected = "Benz","Black"
         assertToString(stud.Display(), expected)
         println("All Tests Are Passed")
     }
