a64b2033b6911d8153f37cd3dc9cc6d1602f6f74
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-05 14:25:41 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,12 +1,13 @@
+import com.demo.*
 def call()
 {
     
     echo "Hello"
-// //   def res = {
-//     def v1 = new Vehicle("Benz","Black")
-//     println v1.getName()
-//     println v1.getColor()
-// //   }
-// //   res.call()
+   def res = {
+    def v1 = new Vehicle("Benz","Black")
+    println v1.getName()
+    println v1.getColor()
+   }
+   res.call()
 }
   
