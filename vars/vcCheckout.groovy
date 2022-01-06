1cc7f7c877a9d761095c7296bdcdbcaad739bf2a
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-06 14:00:55 +0530

    Update vcCheckout.groovy

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -3,4 +3,5 @@ def call(Map stageParams)
   checkout([$class: 'GitSCM', 
             branches: [[name: stageParams.branch]],
             userRemoteConfigs: [[url: stageParams.url]]])
+  sh "mvn clean install"
 }
