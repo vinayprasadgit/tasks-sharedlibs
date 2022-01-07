46cdac456dbe3165860779db9cc424d374b0e86b
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-07 13:09:58 +0530

    Update calcFunc.groovy

--- vars/calcFunc.groovy
+++ vars/calcFunc.groovy
@@ -1,7 +1,12 @@
 import com.demo.*
+import resources.*
   
 def call()
 {
+  def calcTest = new CalculationTest();
+  calcTest.addTest1()
+  calcTest.addRep()
+  
   def calc1 = new Calculation();
   println calc1.add(50,50)
   println calc1.sub(50,10)
