421c129fa80a6dcbadaa9e40963a14a172915dbc
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-07 12:45:00 +0530

    Create Calculation.groovy

---
+++ src/com/demo/Calculation.groovy
@@ -0,0 +1,25 @@
+class Calculation {
+  
+    int add(num1,num2)
+    {
+       return num1+num2
+    }
+     int sub(x,y)
+    {
+       return x-y
+    }
+    static String str(name)
+    {
+        return "My name is "+name
+    }
+
+//    static void main(String[] args) {
+//        def cal1 = new Calculation();
+//        cal1.add()
+//        println cal1.sub()
+//        println Calculation.str()
+//
+//
+//    }
+
+}
