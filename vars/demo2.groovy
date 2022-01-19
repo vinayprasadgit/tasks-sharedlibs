f32392d11deede4d168a190ca7a75fbac25143a0
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-19 14:20:00 +0530

    Update demo2.groovy

--- vars/demo2.groovy
+++ vars/demo2.groovy
@@ -1,4 +1,14 @@
 def call(){
-  sh "tasks-sharedlibs/vars/script.sh"
-  sh "tasks-sharedlibs/vars/script.sh"
+  
+  def content = libraryResource "com/math/mul/script1.sh"
+  writeFile file: "script1.sh", text: content
+  sh "chmod a+x ./script1.sh"
+  sh "./script1.sh"
+  
+  def content = libraryResource "com/math/mul/script2.sh"
+  writeFile file: "script2.sh", text: content
+  sh "chmod a+x ./script2.sh"
+  sh "./script2.sh"
+  
+  
 }
