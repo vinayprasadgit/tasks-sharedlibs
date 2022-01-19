1a4ec0ffdea8c137baae40b6fb695e25e91928f7
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-19 15:36:28 +0530

    Update demo2.groovy

--- vars/demo2.groovy
+++ vars/demo2.groovy
@@ -1,15 +1,15 @@
 def call(x,y)
 {
   
-  def content1 = libraryResource "com/math/mul/scriptDemo.sh"
-  writeFile file: "scriptDemo.sh", text: content1
-  sh "chmod a+x ./scriptDemo.sh"
-  sh "./scriptDemo.sh ${x} ${y}"
+  def content1 = libraryResource "com/math/mul/script1.sh"
+  writeFile file: "script1.sh", text: content1
+  sh "chmod a+x ./script1.sh"
+  sh "./script1.sh ${x} ${y}"
   
-//   def content2 = libraryResource "com/math/mul/script2.sh"
-//   writeFile file: "script2.sh", text: content2
-//   sh "chmod a+x ./script2.sh"
-//   sh "./script2.sh"
+  def content2 = libraryResource "com/math/mul/script2.sh"
+  writeFile file: "script2.sh", text: content2
+  sh "chmod a+x ./script2.sh"
+  sh "./script2.sh"
   
   
 }
