5f8cb7143961219ae6c597544e17204f15d0ab63
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-20 11:00:54 +0530

    Update script1.sh

--- resources/com/math/mul/script1.sh
+++ resources/com/math/mul/script1.sh
@@ -3,11 +3,17 @@
 # export VAL1=4
 # export VAL2=1
 
-env.VAL1=5
-env.VAL2=50
+# env.VAL1=5
+# env.VAL2=50
+
+VAL1=5
+VAL2=50
+
 
 echo "Hello"
 echo "$VAL1"
 echo "$VAL2"
 echo $FNAME
+
+sh ./script2.sh $VAL1 $VAL2
 
