2b00972b905733d8281f4919a37c194a0bd75bed
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-20 14:50:18 +0530

    Update checkRelease.sh

--- resources/com/math/mul/checkRelease.sh
+++ resources/com/math/mul/checkRelease.sh
@@ -5,7 +5,12 @@ then
   if [ -n $2 ]
   then 
     echo "you cannot have a version classifier when deploying"
+#     currentBuild.result='UNSTABLE'
   fi
+else
+  if [ -z $2 ]
+  then
+     echo "Please Specify Version Classifier"
 fi
 echo "$1"
 echo "$dr"
