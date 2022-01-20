797dcc9742a024966b335213e805a96fa5cf59a4
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-20 15:45:36 +0530

    Update checkRelease.sh

--- resources/com/math/mul/checkRelease.sh
+++ resources/com/math/mul/checkRelease.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+deployRelease=$1 
+versionClassifier=$2
 if [ $1 == "true" ]
 then 
   if [ -n $2 ]
@@ -12,7 +13,10 @@ else
      echo "Please Specify Version Classifier"
   fi
 fi
-echo "$1"
-echo "$dr"
-echo "$vc"
-echo "$2"
+# echo "$1"
+# echo "$dr"
+# echo "$vc"
+# echo "$2"
+
+echo "$deployRelease"
+echo "$versionClassifier"
