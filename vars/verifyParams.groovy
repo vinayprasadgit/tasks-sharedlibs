cdcd8592d319ec76c8070465d1faa939a8be0b13
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-20 13:50:35 +0530

    Update verifyParams.groovy

--- vars/verifyParams.groovy
+++ vars/verifyParams.groovy
@@ -1,9 +1,9 @@
-def call(deploy-release,version-classifier)
+def call(deployRelease,versionClassifier)
 {
   def content1 = libraryResource "com/math/mul/checkRelease.sh"
   writeFile file: "checkRelease.sh", text: content1
   sh "chmod a+x ./checkRelease.sh"
-  sh "./checkRelease.sh "$deploy-release" "$version-classifier""
+  sh "./checkRelease.sh "$deployRelease" "$versionClassifier""
 }
   
   
