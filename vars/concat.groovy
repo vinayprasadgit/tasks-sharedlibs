3c05aa411ac5b4e445909400c24faa020a586685
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-21 17:42:12 +0530

    Update concat.groovy

--- vars/concat.groovy
+++ vars/concat.groovy
@@ -1,4 +1,9 @@
 def call(){
-  sh "info.sh"
-  sh "info2.sh"
+ 
+  def val = sh script(bash ./info2.sh ,returnStdout: true).trim().split('\n')
+  
+  println val[0]
+  println val[0]
+  println val[0]
+  
 }
