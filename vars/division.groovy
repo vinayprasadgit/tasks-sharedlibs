89cdb73f5ba8d0c40ff5fbd29bef646437f6eed6
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-02-23 16:17:19 +0530

    Update division.groovy

--- vars/division.groovy
+++ vars/division.groovy
@@ -1,9 +1,10 @@
-def call(val1,val2,val3){
+def call(List<Integer> val){
 
   int mul = 0;
-          def a1 = [val1,val2,val3]
+         
         boolean flag = false;
         for(int i=0;i<a1.size;i++){
+            def a1 = [val[i]]
             if (a1[i] % 7 == 0) {
                 int x;
                 x = a1[i] * 2;
